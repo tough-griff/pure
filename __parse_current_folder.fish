2bfe0e8d4b21bb28250f5a86320912f6bcc99088
Griffin Yourick <gryphon92@gmail.com>
2017-06-30 22:19:20 -0700

    Fix pretty path regex

--- __parse_current_folder.fish
+++ __parse_current_folder.fish
@@ -1,3 +1,3 @@
 function __parse_current_folder -d "Replace '$HOME' with '~', truncate directories"
-  pwd | sed -E -e "s|$HOME|~|" -e "s|(\.?[^\.])[^\/]*\/|\1\/|g"
+  pwd | sed -E -e "s|$HOME|~|" -e "s|(\.?[^\.\/])[^\/]*\/|\1\/|g"
 end
